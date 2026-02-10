9946e922c4929375c44f621c2a0049307853c3f4
koushi hiraoka <hiraoka.koushi@arakawa-lab.com>
2026-02-11 07:57:21 +0900

    add: grants

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -31,6 +31,10 @@ export const grants: { title: string; organization: string; period: string }[] =
         organization: "科学技術振興機構 (JST)", 
         period : "2025年4月 - 現在" 
     },
+    {   title: "2025年度 日本学生支援機構奨学金 大学院「特に優れた業績による返還免除」", 
+        organization: "日本学生支援機構", 
+        period : "2025年度" 
+    },
 ];
 
 export const awards: { title: string; organization: string; year: string; month: string }[] = [
